51ee09b87bdd1a4640514579f580f6e552b1e964
tom-workman <tom.workman@us.ibm.com>
2016-05-20 14:58:05 -0500

    added states

--- public/app.jsx
+++ public/app.jsx
@@ -1,8 +1,47 @@
+var GreeterMessage = React.createClass({
+  render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
+        <div>
+          <h1>Howdy {name}!</h1>
+          <p>{message}!!</p>
+        </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  onFormSubmit: function(e) {
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+
+    if (name.length > 0) {
+      nameRef.value = '';
+      this.props.onNewName(name);
+    }
+
+  },
+  render: function() {
+    return (
+        <div>
+          <form onSubmit={this.onFormSubmit}>
+            <input type="text" ref="name"/>
+            <button>Set Name</button>
+          </form>
+        </div>
+    );
+  }
+});
+
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
       name: 'React',
-      message: 'This is a default message!'
+      message: 'This is a default message and ReactJS is awesome!'
     }
   },
   getInitialState: function() {
@@ -10,38 +49,26 @@ var Greeter = React.createClass({
       name: this.props.name
     };
   },
-  onButtonClick: function(e) {
-    e.preventDefault();
-
-    var nameRef = this.refs.name;
-    var name = nameRef.value;
-    nameRef.value = '';
-
-    if (typeof name === 'string' && name.length > 0) {
-      this.setState({
-        name: name
-      });
-    }
+  handleNewName: function(name) {
+    this.setState({
+      name: name
+    });
   },
   render: function() {
     var name = this.state.name;
     var message = this.props.message;
     return (
       <div>
-        <h1>Howdy {name}!</h1>
-        <p>{message + '!!'}</p>
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
-        </form>
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewName={this.handleNewName} />
       </div>
     );
   }
 });
 
-var firstName = "Thomas";
+var firstName = "Visitor";
 
 ReactDOM.render(
-  <Greeter name={firstName} message="Black Sheep starring Chris Farley is awesome!!"/>,
+  <Greeter name={firstName} />,
   document.getElementById('app')
 );
